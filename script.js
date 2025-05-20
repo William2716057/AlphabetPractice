670f7f2fda77c0941937c599eb5c296ad892dcad
William <willspri@gmail.com>
2025-05-21 09:16:05 +1200

    update

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+let flashCtx, flashcard, ctx, canvas;
+//alphabet
+//display letter function
+function drawLetter() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const letter = letters[Math.floor(Math.random() * letters.length)];
+
+    flashCtx.clearRect(0, 0, flashcard.width, flashcard.height);
+    flashCtx.font = '150px Arial';
+    flashCtx.Style = 'black';
+    flashCtx.Align = 'Center';
+    flashCtx.textBaseline = 'middle';
+    flashCtx.fillText(letter, flashcard.width / 2, flashcard.height / 2);
+}
+
+//slide offscreen
+function slideFlashCard() {
+    flashcard.style.transition = 'transform 0.5s ease';
+    flashcard.style.transform = 'translateX(-400px)';
+
+    setTimeout(() => {
+        slideFlashCard.style.transition = 'none';
+        slideFlashCard.style.transform = 'translateY(-400px)';
+
+        //change to new letter
+        drawLetter();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //animate 
+        requestAnimationFrame(() => {
+            flashcard.style.transition = 'transform 0.5s ease';
+            flashcard.style.transform = 'translateY(0)';
+        });
+    }, 500);
+}
+
+//next button 
+document.querySelector('button:nth-of-type(2)').addEventlistener('click', function () {
+    slideFlashCard();
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const canvas = document.getElementById('box');
     const ctx = canvas.getContext('2d');
@@ -6,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let lastY = 0;
     let isDrawing = false;  // Track mouse up or down
 
-
+    //drawing functions
     canvas.addEventListener('mousedown', function (e) {
         isDrawing = true;  // is pressed
         lastX = e.offsetX;  // update position
@@ -32,10 +74,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // Stop drawing when released
     canvas.addEventListener('mouseup', function () {
         isDrawing = false;  // Mouse button released
+        lastX = e.offsetX;
+        lastY = e.offsetY;
     });
 
 
     canvas.addEventListener('mouseout', function () {
         isDrawing = false;  // Stop drawing if the cursor leaves the canvas
     });
+
+    //starting letter
+    drawLetter();
 });
